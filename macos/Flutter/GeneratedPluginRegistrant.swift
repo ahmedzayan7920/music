a9a051ef17fe235122d2b4cd14d32c067ebc81bb
ahmedzayan7920 <88338596+ahmedzayan7920@users.noreply.github.com>
2024-07-21 14:59:46 +0300

    Add Firebase Core dependency
    The commit adds the `firebase_core` dependency to the `pubspec.yaml` file. This allows the app to integrate with Firebase services, laying the foundation for future Firebase-related features.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import audio_service
 import audio_session
+import firebase_core
 import just_audio
 import package_info_plus
 import path_provider_foundation
@@ -16,6 +17,7 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
